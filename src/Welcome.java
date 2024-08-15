16c2800e15d3ba2919515df9f849eb865f507749
quoc <luuvanquoc9999@gmail.com>
2024-08-15 10:50:26 +0700

    Message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Lưu Văn Quốc!");
+        System.out.println("Welcom to the DHKTPM19A!");
+    }
+}
